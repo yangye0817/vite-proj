7fe61bec2030673e4c846b5db792b8a97690e163
yangye026 <yangye026@ke.com>
2021-04-27 14:42:01 +0800

    init

---
+++ 1.js
@@ -0,0 +1,11 @@
+const { build } = require('./node_modules/vite/dist/node/chunks/dep-5c642f9e.js')
+
+build({
+    root: undefined,
+    base: undefined,
+    mode: undefined,
+    configFile: undefined,
+    logLevel: undefined,
+    clearScreen: undefined,
+    build: {}
+})
---
+++ 1.mjs
@@ -0,0 +1,6 @@
+import a from './2.mjs'
+
+console.log(a)
+
+
+'var __create = Object.create;\nvar __defProp = Object.defineProperty;\nvar __getProtoOf = Object.getPrototypeOf;\nvar __hasOwnProp = Object.prototype.hasOwnProperty;\nvar __getOwnPropNames = Object.getOwnPropertyNames;\nvar __getOwnPropDesc = Object.getOwnPropertyDescriptor;\nvar __markAsModule = (target) => __defProp(target, "__esModule", {value: true});\nvar __export = (target, all) => {\n  for (var name in all)\n    __defProp(target, name, {get: all[name], enumerable: true});\n};\nvar __exportStar = (t…{value: module2, enumerable: true})), module2);\n};\n\n// vite.config.ts\n__markAsModule(exports);\n__export(exports, {\n  default: () => vite_config_default\n});\nvar import_vite = __toModule(require("vite"));\nvar import_plugin_react_refresh = __toModule(require("@vitejs/plugin-react-refresh"));\nvar vite_config_default = (0, import_vite.defineConfig)({\n  plugins: [(0, import_plugin_react_refresh.default)()]\n});\n// Annotate the CommonJS export names for ESM import in node:\n0 && (module.exports = {});\n'
